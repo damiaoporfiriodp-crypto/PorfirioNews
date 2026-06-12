ff355ad2b804104bed2d3d0032487807fd4e2321
Damiao Porfirio <damiaoporfirio.dp@gmail.com>
2026-06-12 05:50:58 -0300

    Migra para PostgreSQL

--- config.py
+++ config.py
@@ -3,6 +3,16 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+
     SECRET_KEY = "porfirionews2026"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database", "porfirionews.db")
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(
+            BASE_DIR,
+            "database",
+            "porfirionews.db"
+        )
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
